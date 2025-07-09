98df90735960a0e89987ba580ba4140785a74f63
Aman Gupta <aman.gupta@bounteous.com>
2025-07-09 12:58:44 +0530

    Increased Responsiveness

--- Bench-Management/src/App.jsx
+++ Bench-Management/src/App.jsx
@@ -3,30 +3,30 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import SignIn from './pages/SignIn';
 import Register from './pages/Register';
-import GlobalNavbar from './components/GlobalNav';
 import BenchHomepage from './pages/BenchHomepage';
 import ManageUsers from './pages/ManageUsers';
 import Dashboard from './pages/Dashboard';
 import AssessmentLanding from './pages/AssessmentSeperateComp/AssessmentHome';
 import AssignAssessment from './pages/AssessmentSeperateComp/AssignAssesment';
+import Layout from './components/Layout';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 
 function App() {
   return (
-    <>
-      <GlobalNavbar />
+    <Routes>
+      {/* Routes without navbar */}
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/register" element={<Register />} />
 
-      <Routes>
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/register" element={<Register />} />
+      {/* Routes with navbar using Layout */}
+      <Route element={<Layout />}>
         <Route path="/home" element={<BenchHomepage />} />
         <Route path="/dashboard/:empId" element={<Dashboard />} />
         <Route path="/manage-users" element={<ManageUsers />} />
         <Route path="/assessmentcomp" element={<AssessmentLanding />} />
         <Route path="/assign-assessment" element={<AssignAssessment />} />
-      </Routes>
-    </>
+      </Route>
+    </Routes>
   );
 }
 
